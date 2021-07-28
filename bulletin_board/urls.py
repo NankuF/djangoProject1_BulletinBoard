7fc5c1590b39bcbc197ea98104ef5188f0e8c1b9
NankuF <fireloki87@gmail.com>
2021-07-28 15:07:01 +0300

    add slug in Bboard, Rubric, User

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -6,7 +6,8 @@ app_name = 'bboard'
 urlpatterns = [
     path('', bboard_index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('<slug:rubric_slug>/', by_rubric, name='by_rubric'),
     path('add/', BbCreateView.as_view(), name='add_ad'),
+    path('<slug:rubric_slug>/', by_rubric, name='by_rubric'),
+
 
 ]
