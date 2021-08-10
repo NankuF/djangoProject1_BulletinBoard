d6ac773fed128594b038add5158a1eaf404f05f5
NankuF <fireloki87@gmail.com>
2021-08-10 22:29:22 +0300

    dz7

--- bulletin_board/models.py
+++ bulletin_board/models.py
@@ -5,6 +5,8 @@ from django.utils.text import slugify
 
 from users.models import CustomUser
 
+from django.utils.functional import cached_property
+
 
 class Rubric(models.Model):
     name = models.CharField(verbose_name='Рубрика', max_length=128, null=True)
@@ -51,3 +53,15 @@ class Bboard(models.Model):
     # не пригодилось
     # def get_absolute_url(self, *args, **kwargs):
     #     return reverse('bboard:by_rubric', kwargs={'rubric_slug': self.slug})
+
+    @cached_property
+    def user_name(self):
+        return self.user.username
+
+    def username_count(self):
+        lst = []
+        for i in self.user_name:
+            lst.append(i)
+        return lst
+
+
--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import bboard_index, contacts, by_rubric, BbCreateView
+from .views import bboard_index, contacts, by_rubric, BbCreateView, see_user_info
 
 app_name = 'bboard'
 
 urlpatterns = [
     path('', bboard_index, name='index'),
+    path('hi/', see_user_info, name='hi'),
     path('contacts/', contacts, name='contacts'),
     path('add/', BbCreateView.as_view(), name='add_ad'),
     path('<slug:rubric_slug>/', by_rubric, name='by_rubric'),
+
 
 
 ]
